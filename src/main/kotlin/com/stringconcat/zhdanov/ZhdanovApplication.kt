a05178ecb4cca355ba0fdf06fe08d15846aab96e
aazhda22 <R0=gvkio>
2021-01-26 15:46:01 +0300

    Initial commit

---
+++ src/main/kotlin/com/stringconcat/zhdanov/ZhdanovApplication.kt
@@ -0,0 +1,11 @@
+package com.stringconcat.zhdanov
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZhdanovApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZhdanovApplication>(*args)
+}
---
+++ src/test/kotlin/com/stringconcat/zhdanov/ZhdanovApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stringconcat.zhdanov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZhdanovApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
